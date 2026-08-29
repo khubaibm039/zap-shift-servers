4a1cb9efd1af10ae53023507501c669117e7017f
khubaibm039 <khubaibm039@gmail.com>
2026-08-30 03:09:25 +0600

    show available parcel to rider

--- index.js
+++ index.js
@@ -181,6 +181,20 @@ async function run() {
                 res.status(500).send({ message: "Failed to fetch parcels" });
             }
         });
+        app.get('/parcels/rider', async (req, res)   => {
+            const {riderEmail, deliveryStatus} =   req.query
+            const query = {}
+            if(riderEmail){
+                query.riderEmail = riderEmail
+            }
+            if(deliveryStatus){
+                query.deliveryStatus = deliveryStatus
+            }
+            const cursor = parcelsCollection.find(query)
+            const result = await cursor.toArray()
+            res.send(result)
+
+        })
 
         app.get("/parcels/:id", async (req, res) => {
             try {
